98f3ffc9375ef26faa9e0db8f21dacd67597bfec
Niumandzi <amazheykin@gmail.com>
2023-11-16 09:50:29 +0300

    small fix

--- pkg/sqlitedb/client.go
+++ pkg/sqlitedb/client.go
@@ -15,28 +15,5 @@ func NewClient(driverName string, filePath string) (*sql.DB, error) {
 }
 
 func CreateTables(db *sql.DB) error {
-	_, err := db.Exec(
-		`CREATE TABLE contact (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			contact_type TEXT CHECK (contact_type IN ('worker', 'private_client', 'legal_client')),
-			name TEXT,
-			number TEXT,
-			email TEXT
-	   );
-
-		CREATE TABLE IF NOT EXISTS hotel (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			name_test VARCHAR(200),
-			location_id INTEGER,
-			number VARCHAR,
-			worker_id INTEGER,
-			description VARCHAR(500),
-			FOREIGN KEY (worker_id) REFERENCES contact (id)
-		)`)
-
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
