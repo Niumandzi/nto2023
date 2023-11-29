c44e30ee469b9a055a70689e5e51df323f31a07a
Niumandzi <amazheykin@gmail.com>
2023-11-29 18:41:19 +0300

    refactor booking model

--- pkg/sqlitedb/client.go
+++ pkg/sqlitedb/client.go
@@ -16,12 +16,14 @@ func NewClient(driverName string, filePath string) (*sql.DB, error) {
 
 func CreateTables(db *sql.DB) error {
 	_, err := db.Exec(
-		`CREATE TABLE IF NOT EXISTS details (
+		`
+		PRAGMA foreign_keys = ON;
+        CREATE TABLE IF NOT EXISTS details (
 			id INTEGER PRIMARY KEY,
 			type_name VARCHAR(255) NOT NULL,
 			category TEXT NOT NULL CHECK (category IN ('entertainment', 'enlightenment', 'education')),
 		    UNIQUE(type_name, category)
-		  );
+		);
 	
 		CREATE TABLE IF NOT EXISTS events (
 		  	id INTEGER PRIMARY KEY,
@@ -30,25 +32,25 @@ func CreateTables(db *sql.DB) error {
 		  	date TEXT,
 		  	details_id INT NOT NULL,
 		  	FOREIGN KEY (details_id) REFERENCES details(id)
-		  );
+		);
 
 		CREATE TABLE IF NOT EXISTS work_type (
 			id INTEGER PRIMARY KEY,
 			name VARCHAR(255) NOT NULL
-		  );
+		);
 
 		CREATE TABLE IF NOT EXISTS facility (
 			id INTEGER PRIMARY KEY,
 			name VARCHAR(255) NOT NULL,
 		    have_parts BOOLEAN
-		  );
+		);
 
-    	CREATE TABLE IF NOT EXISTS part (
-    	    id INTEGER PRIMARY KEY,
+		CREATE TABLE IF NOT EXISTS part (
+			id INTEGER PRIMARY KEY,
 			name VARCHAR(255) NOT NULL,
 			facility_id INT,
-			FOREIGN KEY (facility_id) REFERENCES facility(id)
-    	  );
+			FOREIGN KEY (facility_id) REFERENCES facility(id) ON DELETE CASCADE
+		);
     	
 		CREATE TABLE IF NOT EXISTS application (
 		  	id INTEGER PRIMARY KEY,
@@ -62,7 +64,7 @@ func CreateTables(db *sql.DB) error {
 		    FOREIGN KEY (work_type_id) REFERENCES work_type(id),
 			FOREIGN KEY (event_id) REFERENCES events(id),
 			FOREIGN KEY (facility_id) REFERENCES facility(id)
-		  );
+		);
 
 		CREATE TABLE IF NOT EXISTS booking (
 			id INTEGER PRIMARY KEY,
@@ -75,7 +77,7 @@ func CreateTables(db *sql.DB) error {
 			parts TEXT, -- Список идентификаторов частей помещения
 			FOREIGN KEY (event_id) REFERENCES events(id),
 			FOREIGN KEY (facility_id) REFERENCES facility(id)
-          );
+        );
 	`)
 	if err != nil {
 		return err
